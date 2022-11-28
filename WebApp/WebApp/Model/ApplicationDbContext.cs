a35f68e278021a51493310626673595dd12bfb09
Suvaya <81808968+Suvaya@users.noreply.github.com>
2022-11-28 13:32:11 +0545

    initial commit

---
+++ WebApp/WebApp/Model/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+            
+        public DbSet<Item> Item { get; set; }
+    }
+}
---
+++ WebApp/WebApp/Model/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Model
+{
+    public class Item
+    {
+        [Key]
+
+        public  int Id { get; set; }
+
+        [Required]
+
+        public string Name { get; set; }
+
+        public string Dealer { get; set; }
+
+    }
+}
