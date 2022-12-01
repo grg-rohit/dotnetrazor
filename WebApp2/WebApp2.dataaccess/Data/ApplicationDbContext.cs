c9dfb1bb6ef29ee55639ce30481b9967f0a19cb2
grg-rohit <81430349+grg-rohit@users.noreply.github.com>
2022-12-01 15:50:45 +0545

    deleted migrations

--- WebApp2/WebApp2.dataaccess/Data/ApplicationDbContext.cs
+++ WebApp2/WebApp2.dataaccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebApp2.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace WebApp2.dataaccess.Data
 {
